109300d90f1d120d2b0cb20540bab752eff88070
Wesley <wesleynjerenga9@gmail.com>
2025-05-25 20:20:40 +0300

    More complex SQL query implementations

--- my-python-project/scripts/setup_db.py
+++ my-python-project/scripts/setup_db.py
@@ -1,5 +1,7 @@
 import sqlite3
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from lib.db.connection import get_connection
 
 def setup_database(db_path):
